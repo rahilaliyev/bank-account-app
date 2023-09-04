5887e2723799792ec7808a110c05d3bd06226eb9
Rahil <rahil@globalsoft.az>
2023-09-04 22:52:38 +0400

    error cases and tests for functions

--- src/js/bankAccount.js
+++ src/js/bankAccount.js
@@ -4,11 +4,27 @@ function BankAccount(ownerName, initialBalance) {
 }
 
 BankAccount.prototype.deposit = function (amount) {
+  if (typeof amount !== "number") {
+    throw new Error("Enter a valid number");
+  }
+  if (amount < 0) {
+    throw new Error("Amount must be positive number");
+  }
   this.balance += amount;
 };
 
 BankAccount.prototype.withdraw = function (amount) {
-  this.balance -= amount;
+  if (typeof amount !== "number") {
+    throw new Error("Enter a valid number");
+  }
+  if (amount < 0) {
+    throw new Error("Amount must be positive number");
+  }
+  if (this.balance < amount) {
+    throw new Error("You don't have enough money in your balance");
+  } else {
+    this.balance -= amount;
+  }
 };
 
 BankAccount.prototype.getBalance = function () {
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ import { BankAccount } from "./BankAccount";
 
 function initialize() {
   const rahil = new BankAccount("Rahil", 500);
+  rahil.deposit(50);
+  rahil.withdraw(320);
   console.log(rahil.getBalance());
 }
 
